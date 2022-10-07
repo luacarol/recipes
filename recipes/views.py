7ae90a528b73015067c29f3057f9a6a0808ae3dc
luacarol <lucarolp.deoli@gmail.com>
2022-10-07 19:02:49 -0300

    Começando a criar a estrutura com html

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html')
-
-def sobre(request):
-    return HttpResponse('Sobre')
-
-def contato(request):
-    return HttpResponse('Contato')
+    return render(request, 'home.html', context={
+        'name': 'Luana Caroliny',
+    })
