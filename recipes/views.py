17c501e73702d0973882bd62c4d6b1c52ce2e378
luacarol <lucarolp.deoli@gmail.com>
2022-10-07 18:51:46 -0300

    Criando a pasta de template

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Home')
+    return render(request, 'home.html')
 
 def sobre(request):
     return HttpResponse('Sobre')
