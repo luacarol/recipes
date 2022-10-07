3d1e03ee4cb33e669f01b02dd3c8b0f2ac6738e3
luacarol <lucarolp.deoli@gmail.com>
2022-10-07 18:33:41 -0300

    Migrando as funções para o app

--- project/urls.py
+++ project/urls.py
@@ -1,24 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
-
-# Http request
-def home(request):
-    ...
-    # return Http response
-    return HttpResponse('Home')
-
-# Http request
-def sobre(request):
-    ...
-    # return Http response
-    return HttpResponse('Sobre')
-
-# Http request
-def contato(request):
-    ...
-    # return Http response
-    return HttpResponse('Contato')
+from recipes.views import home, sobre, contato
 
 urlpatterns = [
     path('admin/', admin.site.urls),
